010e41164c1ea256dffcba176e06db482340d045
Esdras <esdras.antonio@outlook.com.br>
2022-05-28 15:33:47 -0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import br.com.dio.model.Gato;
+
+public class Main {
+    public static void main(String[] args) {
+        Gato gato = new Gato("Quarentena","Malhada",2);
+        System.out.println(gato);
+    }
+
+}
